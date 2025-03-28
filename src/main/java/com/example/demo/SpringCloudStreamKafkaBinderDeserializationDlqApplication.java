8d1f0d61f9dff542298db8eef2e83b6081e90807
Pearson Radu <pradu96@gmail.com>
2025-03-28 11:40:47 -0400

    object demo

--- src/main/java/com/example/demo/SpringCloudStreamKafkaBinderDeserializationDlqApplication.java
+++ src/main/java/com/example/demo/SpringCloudStreamKafkaBinderDeserializationDlqApplication.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
+import java.util.function.Consumer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.config.ListenerContainerCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.listener.AbstractMessageListenerContainer;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class SpringCloudStreamKafkaBinderDeserializationDlqApplication {
@@ -19,26 +17,25 @@ public class SpringCloudStreamKafkaBinderDeserializationDlqApplication {
 	}
 
 	@Bean
-	public Consumer<Double> functionName() {
+	Consumer<Event> functionName() {
 		return s -> {
+			if (s.message().contains("w")) {
+				throw new RuntimeException();
+			}
+
 			System.out.println(s);
 		};
 	}
 
 	@Bean
-	public DeadLetterPublishingRecoverer publisher(KafkaOperations template) {
-		return new DeadLetterPublishingRecoverer(template);
+	DefaultErrorHandler errorHandler() {
+		return new DefaultErrorHandler((consumerRecord, exception) -> {
+			System.out.println(consumerRecord.topic() + consumerRecord.partition() + consumerRecord.offset());
+		});
 	}
 
 	@Bean
-	public DefaultErrorHandler errorHandler(DeadLetterPublishingRecoverer deadLetterPublishingRecoverer) {
-		return new DefaultErrorHandler(deadLetterPublishingRecoverer);
-	}
-
-	@Bean
-	public ListenerContainerCustomizer<AbstractMessageListenerContainer<?, ?>> customizer(DefaultErrorHandler errorHandler) {
-		return (container, dest, group) -> {
-			container.setCommonErrorHandler(errorHandler);
-		};
+	ListenerContainerCustomizer<AbstractMessageListenerContainer<?, ?>> customizer(DefaultErrorHandler errorHandler) {
+		return (container, dest, group) -> container.setCommonErrorHandler(errorHandler);
 	}
 }
